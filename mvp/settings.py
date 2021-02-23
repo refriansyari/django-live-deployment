f3fe2f22f594b0c3889845a0c79ec689ff5783a9
refriansyari <65706381+refriansyari@users.noreply.github.com>
2021-02-23 20:55:19 +0700

    Update settings.py

--- mvp/settings.py
+++ mvp/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'mjq)%57%r9t8yj6sq5p5@wtdlat)ii^ljk_#x(_ilbbn+*&=6b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0','eddies-mvp.herokuapp.com','127.0.0.1','localhost']
-# 
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1','0.0.0.0','localhost',]
 
 
 # Application definition
