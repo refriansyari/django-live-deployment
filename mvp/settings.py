77d44736723f94e99c7cdded0e5e74d32dbf479b
refriansyari <65706381+refriansyari@users.noreply.github.com>
2021-02-23 21:38:59 +0700

    Update settings.py

--- mvp/settings.py
+++ mvp/settings.py
@@ -152,7 +152,8 @@ LOGGING = {
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
